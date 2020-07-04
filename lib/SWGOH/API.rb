57597ead68267faa043ca733ca2104aa1b614446
Jonathan Quass <quassj@gmail.com>
2020-07-04 09:16:50 -0400

    Add support for additional request params
    - language
    - enums
    - project

--- lib/SWGOH/API.rb
+++ lib/SWGOH/API.rb
@@ -7,6 +7,12 @@ module SWGOH
   module API
     class Error < StandardError; end
 
+    @access_token = nil
+    @language = LANGUAGE::ENG_US
+    @enums = false
+    @structure = false
+    @base_path = 'https://api.swgoh.help/'
+
     def self.auth(username, password)
       response = HTTP.post(@base_path + PATH::AUTH_SIGNIN, form: {
                              username: username,
@@ -18,6 +24,23 @@ module SWGOH
       json_response = JSON.parse(response)
       @access_token = json_response['access_token']
       true
+    end
+
+    def self.language(language)
+      @language = language
+    end
+
+    def self.enums(enums)
+      @enums = enums
+    end
+
+    def self.structure(structure)
+      @structure = structure
+    end
+
+    # @return [TrueClass, FalseClass]
+    def self.authenticated?
+      @access_token.nil?
     end
 
     def self.get_players(ally_codes)
@@ -52,8 +75,6 @@ module SWGOH
       request(PATH::BATTLES, ally_codes)
     end
 
-    @access_token = nil
-
     module PATH
       # frozen_string_literal: true
       AUTH_SIGNIN = 'auth/signin'
@@ -69,14 +90,33 @@ module SWGOH
       REGISTRATION = 'registration'
     end
 
-    @base_path = 'https://api.swgoh.help/'
+    module LANGUAGE
+      # frozen_string_literal: true
+      CHS_CN = 'CHS_CN'
+      CHT_CN = 'CHT_CN'
+      ENG_US = 'ENG_US'
+      FRE_FR = 'FRE_FR'
+      GER_DE = 'GER_DE'
+      IND_ID = 'IND_ID'
+      ITA_IT = 'ITA_IT'
+      JPN_JP = 'JPN_JP'
+      KOR_KR = 'KOR_KR'
+      POR_BR = 'POR_BR'
+      RUS_RU = 'RUS_RU'
+      SPA_XM = 'SPA_XM'
+      THA_TH = 'THA_TH'
+      TUR_TR = 'TUR_TR'
+    end
 
     def self.request(path, ally_codes)
-      raise Error, 'empty access_token' if @access_token.nil?
+      raise Error, 'Call auth before making requests' if @access_token.nil?
 
       response = HTTP.auth('Bearer ' + @access_token)
                      .post(@base_path + path, form: {
-                             allyCodes: ally_codes
+                             allyCodes: ally_codes,
+                             language: @language,
+                             enums: @enums,
+                             structure: @structure
                            })
       JSON.parse(response)
     end
--- test/SWGOH/API_test.rb
+++ test/SWGOH/API_test.rb
@@ -1,12 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'minitest/autorun'
 
 class SWGOH::APITest < Minitest::Test
-  def test_that_it_has_a_version_number
+  def test_it_has_a_version_number
     refute_nil ::SWGOH::API::VERSION
-  end
-
-  # TODO https://github.com/jquass/SWGOH-API/issues/3
-  def test_it_does_something_useful
-    assert true
   end
 end
